01d87077fd871e58188ad64f239ce333ab91ac3c
MailysGodinaudG21207635 <mailys.godinaud@etu.univ-amu.fr>
2023-06-05 15:28:43 +0200

    base de fxml

--- src/main/java/com/example/sae_201_groupe_d/SeismeApplication.java
+++ src/main/java/com/example/sae_201_groupe_d/SeismeApplication.java
@@ -11,7 +11,7 @@ public class SeismeApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SeismeApplication.class.getResource("seisme-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
