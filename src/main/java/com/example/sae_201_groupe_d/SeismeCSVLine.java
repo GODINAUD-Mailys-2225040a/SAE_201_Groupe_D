8bd39ee053efaab0c19ca056e7bf96b439c67703
MailysGodinaudG21207635 <mailys.godinaud@etu.univ-amu.fr>
2023-06-09 10:04:10 +0200

    ajout position sur map

--- src/main/java/com/example/sae_201_groupe_d/SeismeCSVLine.java
+++ src/main/java/com/example/sae_201_groupe_d/SeismeCSVLine.java
@@ -48,9 +48,9 @@ public class SeismeCSVLine //Challah ça marche, à tester
     {
         try (FileInputStream file = new FileInputStream("SAE_201_Groupe_D/src/main/resources/com/example/sae_201_groupe_d/SisFrance_seismes_20230605145730.csv")) {
             System.out.println("file ok");
-       //     while (file.()) {
-         //       usableList.add(getRecordFromLine(file.nextLine()));
-           // }
+            while (file.()) {
+                usableList.add(getRecordFromLine(file.nextLine()));
+            }
         } catch (Exception failed) {System.out.println("file failed");}
     }
 
